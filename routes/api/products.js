c1c7e9896608ee85e0af0a41a1f835d6d563a80f
robert hunnicutt <robhunn74@gmail.com>
2018-08-02 07:19:27 -0500

    product curd almost done

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
